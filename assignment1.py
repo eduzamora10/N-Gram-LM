293fea4694ced6e083968053c1032bb5527dd693
dcsajdak <dcsajdak@gmail.com>
2025-02-04 12:35:14 -0600

    code structure

--- assignment1.py
+++ assignment1.py
@@ -1,10 +1,33 @@
-# code for unigram and bigram models
+class NGramModel:
+    def __init__(self, train, val):
+        self.train_set = train
+        self.val_set = val
+        pass
+    
+    # code for unigram model
+    def unigram_model():
+        pass
+    
+    # code for bigram model
+    def bigram_model():
+        pass
+        
+    # code for smoothing (laplace)
+    def smoothing():
+        pass
+    
+    # code for unknown word handling
+    def unknown_word_handling():
+        pass
+    
+    # code for calculating perplexity
+    def calculate_perplexity():
+        pass
 
-
-
-# code for smoothing (laplace) and unknown word handling
-
-
-
-# code for perplexity calculation
-
+if __name__ == "__main__":
+    train = "train.txt"
+    val = "val.txt"
+    
+    model = NGramModel(train, val)
+    
+    # call models and evaluation functions
